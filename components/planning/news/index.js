02c6f4ffbedd1959a7d5445cb61d72af0f80a17f
Evgheni <86593030+nev-front-dev@users.noreply.github.com>
2022-06-20 11:42:14 +0300

    create of planning sections

--- components/planning/news/index.js
+++ components/planning/news/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import styles from "./News.module.scss";
-import Link from "next/link";
+// import Link from "next/link";
 import Image from "next/image";
 import location__bg from "../../../public/static/planning-page/news/january/jan-1.jpeg";
 
 export default function index() {
-  const [isSelect, setIsSelect] = React.useState('');
-  const [mounth, setMounth] = React.useState('')
-  // const years=['2022','2021']
-  const mounthes= [
+  const mounthes = [
     "January",
     "February",
     "March",
@@ -20,13 +17,8 @@ export default function index() {
     "September",
     "October",
     "November",
-    "December"
-  ]
-  // let lastMounth = ''
-  const lastMounth = () => {
-    console.log(mounthes[mounthes.length - 1])
-  }
-  lastMounth()
+    "December",
+  ];
   return (
     <>
       <div className={styles.news__section}>
@@ -43,16 +35,15 @@ export default function index() {
               </select>
               <div className={styles.news__navigation_line}>
                 <div className={styles.navigation__items}>
-                  {
-                    mounthes.map(mounth=>(
-                      <div key={mounth} className={styles.navigation__item}>
-                        <p className={styles.navigation__item_name}>{mounth}</p>
-                        <p className={styles.navigation__item_visible}>
-                          
-                        </p>
-                        <button className={styles.navigation__item_button}></button>
-                      </div>
-                    ))}
+                  {mounthes.map((mounth) => (
+                    <div key={mounth} className={styles.navigation__item}>
+                      <p className={styles.navigation__item_name}>{mounth}</p>
+                      <p className={styles.navigation__item_visible}></p>
+                      <button
+                        className={styles.navigation__item_button}
+                      ></button>
+                    </div>
+                  ))}
                 </div>
               </div>
               <select className={styles.news__navigation_mounth_mob}>
@@ -61,7 +52,7 @@ export default function index() {
               </select>
             </div>
             <div className={styles.news__grids}>
-              <January/>
+              <January />
               {/* <Febuary/> */}
             </div>
           </div>
@@ -74,102 +65,53 @@ export default function index() {
 function January() {
   return (
     <div className={styles.news__grid_month}>
-                <div className={styles.news__grid_item}>
-                  <div className={styles.item__bg}>
-                    <Image
-                      src={location__bg}
-                      height={333}
-                      width={499}
-                      priority
-                      alt=""
-                    />
-                  </div>
-                </div>
-                <div className={styles.news__grid_item}>
-                  <div className={styles.item__bg}>
-                    <Image
-                      src={location__bg}
-                      height={333}
-                      width={499}
-                      priority
-                      alt=""
-                    />
-                  </div>
-                </div>
-                <div className={styles.news__grid_item}>
-                  <div className={styles.item__bg}>
-                    <Image
-                      src={location__bg}
-                      height={333}
-                      width={499}
-                      priority
-                      alt=""
-                    />
-                  </div>
-                </div>
-                <div className={styles.news__grid_item}>
-                  <div className={styles.item__bg}>
-                    <Image
-                      src={location__bg}
-                      height={333}
-                      width={499}
-                      priority
-                      alt=""
-                    />
-                  </div>
-                </div>
-              </div>
-  )
+      <div className={styles.news__grid_item}>
+        <div className={styles.item__bg}>
+          <Image src={location__bg} height={333} width={499} priority alt="" />
+        </div>
+      </div>
+      <div className={styles.news__grid_item}>
+        <div className={styles.item__bg}>
+          <Image src={location__bg} height={333} width={499} priority alt="" />
+        </div>
+      </div>
+      <div className={styles.news__grid_item}>
+        <div className={styles.item__bg}>
+          <Image src={location__bg} height={333} width={499} priority alt="" />
+        </div>
+      </div>
+      <div className={styles.news__grid_item}>
+        <div className={styles.item__bg}>
+          <Image src={location__bg} height={333} width={499} priority alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-function Febuary() {
-  return (
-    <div className={styles.news__grid_month}>
-                <div className={styles.news__grid_item}>
-                  <div className={styles.item__bg}>
-                    <Image
-                      src={location__bg}
-                      height={333}
-                      width={499}
-                      priority
-                      alt=""
-                    />
-                  </div>
-                </div>
-                <div className={styles.news__grid_item}>
-                  <div className={styles.item__bg}>
-                    <Image
-                      src={location__bg}
-                      height={333}
-                      width={499}
-                      priority
-                      alt=""
-                    />
-                  </div>
-                </div>
-                <div className={styles.news__grid_item}>
-                  <div className={styles.item__bg}>
-                    <Image
-                      src={location__bg}
-                      height={333}
-                      width={499}
-                      priority
-                      alt=""
-                    />
-                  </div>
-                </div>
-                <div className={styles.news__grid_item}>
-                  <div className={styles.item__bg}>
-                    <Image
-                      src={location__bg}
-                      height={333}
-                      width={499}
-                      priority
-                      alt=""
-                    />
-                  </div>
-                </div>
-              </div>
-  )
-}
-
+// function Febuary() {
+//   return (
+//     <div className={styles.news__grid_month}>
+//       <div className={styles.news__grid_item}>
+//         <div className={styles.item__bg}>
+//           <Image src={location__bg} height={333} width={499} priority alt="" />
+//         </div>
+//       </div>
+//       <div className={styles.news__grid_item}>
+//         <div className={styles.item__bg}>
+//           <Image src={location__bg} height={333} width={499} priority alt="" />
+//         </div>
+//       </div>
+//       <div className={styles.news__grid_item}>
+//         <div className={styles.item__bg}>
+//           <Image src={location__bg} height={333} width={499} priority alt="" />
+//         </div>
+//       </div>
+//       <div className={styles.news__grid_item}>
+//         <div className={styles.item__bg}>
+//           <Image src={location__bg} height={333} width={499} priority alt="" />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
