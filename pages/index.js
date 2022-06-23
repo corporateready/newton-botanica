230263a643ce8260fd1de1d93e91c14f70e09c3f
Evgheni <86593030+nev-front-dev@users.noreply.github.com>
2022-06-23 20:34:28 +0300

    change bg image format in home page

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ import Image from "next/image";
 import header__home_logo from "../public/static/home-page/header-logo.svg";
 import image__side from "../public/static/home-page/home-image-inner.svg";
 import image__side_mobile from "../public/static/home-page/home-image-inner-mobile.svg";
-import home__bg from "../public/static/home-page/background.webp";
-import home__bg_mobile from "../public/static/home-page/background.webp";
+import home__bg from "../public/static/home-page/background.png";
+// import home__bg_mobile from "../public/static/home-page/background.webp";
 import close__button from "../public/static/planning-page/close-button-popup.svg";
 
 import phone__icon from "../public/static/home-page/phone.svg";
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./pages/**/*.{html,js,ts,jsx,tsx}",
-    "./components/**/*.{html,js,ts,jsx,tsx}",
+    "./components/**/*.{html,scss,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
