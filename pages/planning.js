3693931931c9eaf964fa7fe89ecace7034c989f1
Evgheni <86593030+nev-front-dev@users.noreply.github.com>
2022-06-23 19:36:10 +0300

    created popup for hero page, created terms and policy pages

--- pages/planning.js
+++ pages/planning.js
@@ -1,50 +1,51 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styles from "../styles/Plannig.module.scss";
-import Layout from "../components/layout";
+// import Layout from "../components/layout";
 import Image from "next/image";
 import planning__bg from "../public/static/planning-page/Group 2678.png";
-import {
-  Hero,
-  Complex,
-  Designs,
-  Location,
-  Payment,
-  News,
-} from "../components/planning";
-import OfferPopUp from "../components/popup-offer";
-import PDFOfferPopUp from "../components/popup-pdf-offer";
-import PhoneOfferPopUp from "../components/popup-phone-offer";
+// import {
+//   Hero,
+//   Complex,
+//   Designs,
+//   Location,
+//   Payment,
+//   News,
+// } from "../components/planning";
+// import OfferPopUp from "../components/popup-offer";
+// import PDFOfferPopUp from "../components/popup-pdf-offer";
+// import PhoneOfferPopUp from "../components/popup-phone-offer";
 
 export default function planning() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isPDFOpen, setIsPDFOpen] = useState(false);
-  const [isPhoneOfferOpen, setIsPhoneOfferOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
+  // const [isPDFOpen, setIsPDFOpen] = useState(false);
+  // const [isPhoneOfferOpen, setIsPhoneOfferOpen] = useState(false);
 
-  useEffect(() => {
-    if (isOpen) {
-      document.body.style.overflow = "hidden";
-    }
-    if (!isOpen) {
-      document.body.style.overflow = "auto";
-    }
-  }, [isOpen]);
+  // useEffect(() => {
+  //   if (isOpen) {
+  //     document.body.style.overflow = "hidden";
+  //   }
+  //   if (!isOpen) {
+  //     document.body.style.overflow = "auto";
+  //   }
+  // }, [isOpen]);
 
-  useEffect(() => {
-    if (isPDFOpen) {
-      document.body.style.overflow = "hidden";
-    }
-    if (!isPDFOpen) {
-      document.body.style.overflow = "auto";
-    }
-  }, [isPDFOpen]);
-  useEffect(() => {
-    if (isPhoneOfferOpen) {
-      document.body.style.overflow = "hidden";
-    }
-    if (!isPhoneOfferOpen) {
-      document.body.style.overflow = "auto";
-    }
-  }, [isPhoneOfferOpen]);
+  // useEffect(() => {
+  //   if (isPDFOpen) {
+  //     document.body.style.overflow = "hidden";
+  //   }
+  //   if (!isPDFOpen) {
+  //     document.body.style.overflow = "auto";
+  //   }
+  // }, [isPDFOpen]);
+  
+  // useEffect(() => {
+  //   if (isPhoneOfferOpen) {
+  //     document.body.style.overflow = "hidden";
+  //   }
+  //   if (!isPhoneOfferOpen) {
+  //     document.body.style.overflow = "auto";
+  //   }
+  // }, [isPhoneOfferOpen]);
 
   return (
     <section className={styles.planning__section}>
@@ -57,7 +58,7 @@ export default function planning() {
           alt="home background"
         />
       </div>
-      <Layout>
+      {/* <Layout>
         <div className={styles.container}>
           <Hero
             setIsPDFOpen={setIsPDFOpen}
@@ -74,7 +75,7 @@ export default function planning() {
       {isPDFOpen && <PDFOfferPopUp setIsPDFOpen={setIsPDFOpen} />}
       {isPhoneOfferOpen && (
         <PhoneOfferPopUp setIsPhoneOfferOpen={setIsPhoneOfferOpen} />
-      )}
+      )} */}
     </section>
   );
 }
