01b154281e8427bce936de36053b3191e90f8b54
Evgheni <86593030+nev-front-dev@users.noreply.github.com>
2022-06-24 16:29:00 +0300

    created thanks page

---
+++ components/thanks-layout/header/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./Header.module.scss";
+import header__logo from "../../../public/static/home-page/header-logo.svg";
+import phone__icon from "../../../public/static/home-page/phone.svg";
+
+export default function index() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.header__inner}>
+          <Link href="/">
+            <a className={styles.header__logo}>
+              <Image
+                src={header__logo}
+                height={54}
+                width={108}
+                priority
+                alt="home header logo"
+              />
+            </a>
+          </Link>
+
+          <Link href="tel:+3736082020">
+            <a className={styles.header__phone_link}>
+              <div className={styles.header__phone_icon}>
+                <Image
+                  src={phone__icon}
+                  height={19}
+                  width={20}
+                  alt="home header phone icon"
+                />
+              </div>
+              <span className={styles.header__phone_link_number}>
+                + 373 60 80 20 20
+              </span>
+            </a>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
--- pages/thanks.js
+++ pages/thanks.js
@@ -1,13 +1,46 @@
-import Link from 'next/link'
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import styles from "../styles/Thanks.module.scss";
+import Layout from "../components/thanks-layout";
+// import video from '../public/video/video-1.mp4'
 
 export default function Thanks() {
   return (
-    <div className='flex justify-center items-center h-screen w-full relative'>
-        <Link href="/">
-            <a className='inline-block absolute top-[5%] left-[5%] hover:scale-105 duration-200'>HOME</a>
-        </Link>
-        <h1 className=' text-[50px] font-semibold'>Thanks</h1>
+    <div className={styles.thanks__screen}>
+      <Layout>
+        <div className="flex justify-center items-center h-screen w-full relative mt-[7%]">
+          <div className="flex flex-col">
+            <h1 className="text-[50px] text-white leading-none text-center font-semibold">
+              Thank you!
+            </h1>
+            <div className={styles.video__wrapper}>
+              <Video />
+            </div>
+            <Link href="/">
+              <a className="text-white border py-[3%] mt-[-3%] w-[50%] mx-auto rounded-md text-center hover:bg-white hover:text-[#2BB018] duration-200">
+                HOME
+              </a>
+            </Link>
+          </div>
+        </div>
+      </Layout>
     </div>
-  )
+  );
+}
+
+function Video() {
+  return (
+    <div>
+      <iframe
+        className="w-full aspect-video"
+        width="600"
+        height="280"
+        src="https://player.vimeo.com/external/394180138.sd.mp4?s=a5512e7c2923c4f290018495cc65f856f4e37259&profile_id=165&oauth2_token_id=57447761"
+        title="Soft Music Radio — Chillout Station — Work, Study, Relax"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+    </div>
+  );
 }
