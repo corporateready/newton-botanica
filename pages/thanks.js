8ed954c4a2d7b31973fa3984ae50380b185f9116
Evgheni <86593030+nev-front-dev@users.noreply.github.com>
2022-07-14 11:24:50 +0300

    created plugin for fast load you tube on thanks page

--- pages/thanks.js
+++ pages/thanks.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
 import styles from "../styles/Thanks.module.scss";
 import Layout from "../components/thanks-layout";
@@ -58,17 +58,34 @@ export default function Thanks() {
 }
 
 function YoutubeVideo() {
+
+  useEffect(()=>{
+    document.addEventListener('fetch', event => {
+      event.respondWith(async function () {
+          // Respond from the cache if we can
+          const cachedResponse = await caches.match(event.request);
+          if (cachedResponse) return cachedResponse; // Else, use the preloaded response, if it's there
+          const response = await event.preloadResponse;
+          if (response) return response; // Else try the network.
+          return fetch(event.request);
+      }());
+  });
+  }, [])
+
   const onPlayerReady = (event) => {
     event.target.pauseVideo();
   };
+
+
 
   const opts = {
     height: "590px",
     width: "100%",
     loading:"lazy",
     playerVars: {
+      passive: true,
       // https://developers.google.com/youtube/player_parameters
-      // autoplay: 1,
+      autoplay: 1,
     },
   };
 
