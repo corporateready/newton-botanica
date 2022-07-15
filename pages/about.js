49a298422fcf7b0b6cdd3e148e6fb1fa353d9eef
Evgheni <86593030+nev-front-dev@users.noreply.github.com>
2022-07-15 11:09:41 +0300

    corretion of background on about page

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,7 @@ import styles from "../styles/Plannig.module.scss";
 import Layout from "../components/about/layout";
 import Image from "next/image";
 import planning__bg from "/public/static/planning-page/Group_2678.webp";
+import planning__bg_mob from "/public/static/planning-page/bg-group-mob.png";
 import {
   Hero,
   Complex,
@@ -15,12 +16,11 @@ import {
 import Head from "next/head";
 
 export default function Planning() {
-
   return (
     <>
       <Head>
-      <link rel="prefetch"/>
-      <link rel="preconnect" href="http://localhost:3000/about/" />
+        <link rel="prefetch" />
+        <link rel="preconnect" href="http://localhost:3000/about/" />
         <title>NGNB | PLANIMETRII</title>
       </Head>
 
@@ -29,9 +29,15 @@ export default function Planning() {
           <Image
             src={planning__bg}
             layout="fill"
-            // height={6000}
-            // width={1440}
-            // objectFit="fill"
+            quality={100}
+            priority
+            alt="home background"
+          />
+        </div>
+        <div className={styles.planning__bg_mob}>
+          <Image
+            src={planning__bg_mob}
+            layout="fill"
             quality={100}
             priority
             alt="home background"
@@ -42,7 +48,7 @@ export default function Planning() {
             <Hero />
             <Complex />
             <Designs />
-            <SliderSnake/>
+            <SliderSnake />
             <Location />
             <Payment />
             <News />
