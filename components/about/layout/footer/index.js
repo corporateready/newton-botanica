142e864939f5e9574bf2a180cbecb0809feabc96
Evgheni <86593030+nev-front-dev@users.noreply.github.com>
2022-07-25 13:35:06 +0300

    correction of footer on about page

--- components/about/layout/footer/index.js
+++ components/about/layout/footer/index.js
@@ -6,45 +6,51 @@ import footer__logo from "../../../../public/static/footer/logo-footer.svg";
 import phone__icon from "../../../../public/static/footer/phone-icon.svg";
 import loc__icon from "../../../../public/static/footer/location.svg";
 import mail__icon from "../../../../public/static/footer/mail-icon.svg";
-import {FiInstagram} from '@react-icons/all-files/fi/FiInstagram'
-import {FaFacebookF} from '@react-icons/all-files/fa/FaFacebookF'
-import {FaYoutube} from '@react-icons/all-files/fa/FaYoutube'
+import { FiInstagram } from "@react-icons/all-files/fi/FiInstagram";
+import { FaFacebookF } from "@react-icons/all-files/fa/FaFacebookF";
+import { FaYoutube } from "@react-icons/all-files/fa/FaYoutube";
 
 export default function index() {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
         <div className={styles.footer__inner}>
-          <Link href="/">
-            <a className={styles.footer__logo}>
-              <Image
-                src={footer__logo}
-                height={86}
-                width={171}
-                priority
-                alt="home header logo"
-              />
-            </a>
-          </Link>
+          <div className={styles.footer__logo}>
+            <Image
+              src={footer__logo}
+              height={86}
+              width={171}
+              priority
+              alt="home header logo"
+            />
+          </div>
           <div className={styles.footer__socials}>
-            <p className={styles.social__item}>
-              <span className={styles.social__item_icon}>
-                <FiInstagram size={24} />
-              </span>
-            </p>
-            <p className={styles.social__item}>
-            <span className={styles.social__item_icon}>
-                <FaFacebookF size={24} />
-              </span>
-            </p>
-            <p className={styles.social__item}>
-            <span className={styles.social__item_icon}>
-                <FaYoutube size={24} />
-              </span>
-            </p>
+            <Link href={"https://www.instagram.com/newtonhouse.md/"}>
+              <a className={styles.social__item}>
+                <span className={styles.social__item_icon}>
+                  <FiInstagram size={30} />
+                </span>
+              </a>
+            </Link>
+            <Link href={"https://www.facebook.com/NewtonHouse.md/"}>
+              <a className={styles.social__item}>
+                <span className={styles.social__item_icon}>
+                  <FaFacebookF size={24} />
+                </span>
+              </a>
+            </Link>
+            <Link
+              href={"https://www.youtube.com/channel/UCEOJ9KTW-kbMUnb_r_wVLbQ"}
+            >
+              <a className={styles.social__item}>
+                <span className={styles.social__item_icon_YT}>
+                  <FaYoutube size={25} />
+                </span>
+              </a>
+            </Link>
           </div>
           <div className={styles.footer__contact}>
-            <div className={styles.footer__contact_item}>
+            <a className={styles.footer__contact_item} href="tel:37360802020">
               <span className={styles.footer__contact_icon}>
                 <Image
                   src={phone__icon}
@@ -55,8 +61,9 @@ export default function index() {
                 />
               </span>
               <span className={styles.footer__contact_text}>060 80 20 20</span>
-            </div>
-            <div className={styles.footer__contact_item}>
+            </a>
+            <Link href="https://www.google.com/maps/dir/47.7626368,28.9898496/newton+house+google+map+location/@47.3618199,28.3867357,9.42z/data=!4m9!4m8!1m1!4e1!1m5!1m1!1s0x40c97dc3398a2559:0x71d4f48a93ea6096!2m2!1d28.8776605!2d46.9768761">
+            <a className={styles.footer__contact_item}>
               <span className={styles.footer__contact_icon}>
                 <Image
                   src={loc__icon}
@@ -69,8 +76,9 @@ export default function index() {
               <span className={styles.footer__contact_text}>
                 str. Grădina Botanică, 18
               </span>
-            </div>
-            <div className={styles.footer__contact_item}>
+            </a>
+            </Link>
+            <a className={styles.footer__contact_item} href="mailto:botanica@mail.com">
               <span className={styles.footer__contact_icon}>
                 <Image
                   src={mail__icon}
@@ -83,7 +91,7 @@ export default function index() {
               <span className={styles.footer__contact_text}>
                 newton botanica@gmail.com
               </span>
-            </div>
+            </a>
           </div>
         </div>
         <p className={styles.footer__bottom}>
