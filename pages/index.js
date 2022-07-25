ed74c91e176e7d09c6cc7879c02b23c863f1f64e
Evgheni <86593030+nev-front-dev@users.noreply.github.com>
2022-07-25 12:09:11 +0300

    created country filter in form submit input on home page

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,9 @@ import close__button from "../public/static/planning-page/close-button-popup.svg
 import phone__icon from "../public/static/home-page/phone.svg";
 import Messengers from "../components/messengers";
 
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
+
 export default function Home() {
   const [isOfferOpen, setIsOfferOpen] = React.useState(false);
 
@@ -21,8 +24,13 @@ export default function Home() {
   return (
     <>
       <Head>
-      <link rel="preload" href="globals.scss" as="style" onLoad="this.rel='stylesheet'"/>
-      <link rel="preconnect" href="http://localhost:3000/" />
+        <link
+          rel="preload"
+          href="globals.scss"
+          as="style"
+          onLoad="this.rel='stylesheet'"
+        />
+        <link rel="preconnect" href="http://localhost:3000/" />
         <title>Newton House Grădina Botanica</title>
         <meta
           name="description"
@@ -111,16 +119,13 @@ export default function Home() {
               <div className={styles.desktop__messengers}>
                 <Messengers />
               </div>
-
             </div>
-            
           </div>
-          
         </div>
 
         <div className={styles.mobile__messengers}>
-              <Messengers />
-            </div>
+          <Messengers />
+        </div>
 
         {isOfferOpen && (
           <OfferPopUpSending hanlerClosePopup={hanlerClosePopup} />
@@ -165,71 +170,81 @@ function OfferPopUpSending({ hanlerClosePopup }) {
   }, [isSend]);
 
   return (
-    <section className={styles.offer__section}>
-      <div className={styles.offer__container}>
-        <div className={styles.offer__inner}>
-          <button
-            type="button"
-            className={styles.offer__button_close}
-            onClick={hanlerClosePopup}
-          >
-            <Image
-              src={close__button}
-              height={13}
-              width={13}
-              alt="close button icon"
-            />
-          </button>
-          <h2 className={styles.offer__title}>
-            OBȚINE CONSULTAȚIA MANAGERULUI
-          </h2>
-          <h3 className={styles.offer__subtitle}>
-            Expediază-ne datele de contact și revenim cu un apel în curând
-          </h3>
+    <>
+      <Head></Head>
 
-          <form
-            className={styles.offer__form}
-            action="https://formsubmit.co/nev30inbox@gmail.com"
-            method="POST"
-            onSubmit={useHandlerOnClickToSend}
-          >
-            <input
-              type="text"
-              value={nameValue}
-              onChange={(e) => setNameValue(e.target.value)}
-              placeholder="NUMELE, PRENUMELE"
-              required
-            />
-            <input
-              type="text"
-              value={emailValue}
-              onChange={(e) => setEmailValue(e.target.value)}
-              placeholder="Numărul de telefon"
-              required
-            />
-            <input type="hidden" name="_captcha" value="false" />
-            <input type="hidden" name="_next" value="false" />
-            <input
-              type="hidden"
-              name="_autoresponse"
-              value="Everyone is important for as!"
-            />
-            <button type="submit" className={styles.button__sending}>
-              AFLĂ DETALII
+      <section className={styles.offer__section}>
+        <div className={styles.offer__container}>
+          <div className={styles.offer__inner}>
+            <button
+              type="button"
+              className={styles.offer__button_close}
+              onClick={hanlerClosePopup}
+            >
+              <Image
+                src={close__button}
+                height={13}
+                width={13}
+                alt="close button icon"
+              />
             </button>
-          </form>
+            <h2 className={styles.offer__title}>
+              OBȚINE CONSULTAȚIA MANAGERULUI
+            </h2>
+            <h3 className={styles.offer__subtitle}>
+              Expediază-ne datele de contact și revenim cu un apel în curând
+            </h3>
 
-          <div className={styles.terms__policy}>
-            <Link href="/terms">
-              <a className={styles.terms}>Terms and Conditions </a>
-            </Link>
-            and
-            <Link href="/policy">
-              <a className={styles.policy}> Privacy Policy</a>
-            </Link>
+            <form
+              className={styles.offer__form}
+              action="https://formsubmit.co/nev30inbox@gmail.com"
+              method="POST"
+              onSubmit={useHandlerOnClickToSend}
+            >
+              <input
+                type="text"
+                value={nameValue}
+                onChange={(e) => setNameValue(e.target.value)}
+                placeholder="NUMELE, PRENUMELE"
+                required
+              />
+              <PhoneInput
+                className={styles.input}
+                style={{
+                  height: "auto",
+                  width: "100%",
+                  paddingTop: "0",
+                  paddingBottom: "0",
+                }}
+                placeholder="Numărul de telefon"
+                country={"md"}
+                value={emailValue}
+                onChange={setEmailValue}
+              />
+              <input type="hidden" name="_captcha" value="false" />
+              <input type="hidden" name="_next" value="false" />
+              <input
+                type="hidden"
+                name="_autoresponse"
+                value="Everyone is important for as!"
+              />
+              <button type="submit" className={styles.button__sending}>
+                AFLĂ DETALII
+              </button>
+            </form>
+
+            <div className={styles.terms__policy}>
+              <Link href="/terms">
+                <a className={styles.terms}>Terms and Conditions </a>
+              </Link>
+              and
+              <Link href="/policy">
+                <a className={styles.policy}> Privacy Policy</a>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
