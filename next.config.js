fe9a388904c1a29046a931ecf077665ddbe04384
Evgheni <86593030+nev-front-dev@users.noreply.github.com>
2022-07-28 17:04:24 +0300

    correction preload of Home.module.scss on home page

--- next.config.js
+++ next.config.js
@@ -1,55 +1,11 @@
-const regexEqual = (x, y) => {
-  return (
-    x instanceof RegExp &&
-    y instanceof RegExp &&
-    x.source === y.source &&
-    x.global === y.global &&
-    x.ignoreCase === y.ignoreCase &&
-    x.multiline === y.multiline
-  )
-}
-
-// Overrides for css-loader plugin
-function cssLoaderOptions(modules) {
-  const { getLocalIdent, ...others } = modules // Need to delete getLocalIdent else localIdentName doesn't work
-  return {
-    ...others,
-    localIdentName: '[hash:base64:6]',
-    exportLocalsConvention: 'camelCaseOnly',
-    mode: 'local',
-  }
-}
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    const oneOf = config.module.rules.find((rule) => typeof rule.oneOf === 'object')
-
-    if (oneOf) {
-      // Find the module which targets *.scss|*.sass files
-      const moduleSassRule = oneOf.oneOf.find((rule) =>
-        regexEqual(rule.test, /\.module\.(scss|sass)$/)
-      )
-
-      if (moduleSassRule) {
-        // Get the config object for css-loader plugin
-        const cssLoader = moduleSassRule.use.find(({ loader }) => loader.includes('css-loader'))
-        if (cssLoader) {
-          cssLoader.options = {
-            ...cssLoader.options,
-            modules: cssLoaderOptions(cssLoader.options.modules),
-          }
-        }
-      }
-    }
-
-    return config
-  },
   reactStrictMode: true,
   key: 'X-DNS-Prefetch-Control',
   value: 'on',
   images: {
     formats: ['image/webp'],
-    domains:['swiperjs.com','youtu.be', 'lorempixel.com','botanica.newton.md'],
+    domains:['swiperjs.com','youtu.be', 'lorempixel.com'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   experimental: {
